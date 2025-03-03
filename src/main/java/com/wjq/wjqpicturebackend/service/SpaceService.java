d421f9c4e4d160385dffb97e4d108860fb6be021
Ersami <2381087337@qq.com>
2025-03-03 23:22:01 +0800

    项目提交

---
+++ src/main/java/com/wjq/wjqpicturebackend/service/SpaceService.java
@@ -0,0 +1,13 @@
+package com.wjq.wjqpicturebackend.service;
+
+import generator.domain.Space;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author wjq23
+* @description 针对表【space(空间)】的数据库操作Service
+* @createDate 2025-03-03 22:31:16
+*/
+public interface SpaceService extends IService<Space> {
+
+}
---
+++ src/main/java/com/wjq/wjqpicturebackend/service/impl/SpaceServiceImpl.java
@@ -0,0 +1,22 @@
+package com.wjq.wjqpicturebackend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import generator.domain.Space;
+import com.wjq.wjqpicturebackend.service.SpaceService;
+import com.wjq.wjqpicturebackend.mapper.SpaceMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author wjq23
+* @description 针对表【space(空间)】的数据库操作Service实现
+* @createDate 2025-03-03 22:31:16
+*/
+@Service
+public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
+    implements SpaceService{
+
+}
+
+
+
+
