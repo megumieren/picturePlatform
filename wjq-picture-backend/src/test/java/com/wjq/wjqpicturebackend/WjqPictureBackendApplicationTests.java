d6dcf53b2894cee17f63ed32689680ff9a3ff2f7
Ersami <2381087337@qq.com>
2025-04-18 17:31:22 +0800

    空间页面修改

--- wjq-picture-backend/src/test/java/com/wjq/wjqpicturebackend/WjqPictureBackendApplicationTests.java
+++ wjq-picture-backend/src/test/java/com/wjq/wjqpicturebackend/WjqPictureBackendApplicationTests.java
@@ -1,15 +1,18 @@
 package com.wjq.wjqpicturebackend;
 
-import com.wjq.wjqpicturebackend.model.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.File;
+
 
 @SpringBootTest
 class WjqPictureBackendApplicationTests {
 
     @Test
     void contextLoads() {
+        String property = System.getProperty("user.dir");
 
+        System.out.println(property);
     }
 }
