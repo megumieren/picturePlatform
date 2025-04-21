263be3ec3d6e5b350d7ebf310a772ccfc7388f81
Ersami <2381087337@qq.com>
2025-04-21 15:56:41 +0800

    改为线上设置

--- wjq-picture-frontend/src/utils/pictureEditWebSocket.ts
+++ wjq-picture-frontend/src/utils/pictureEditWebSocket.ts
@@ -15,7 +15,7 @@ export default class PictureEditWebSocket {
   connect() {
     const DEV_BASE_URL = "ws://localhost:8123";
     // 线上地址
-    const PROD_BASE_URL = "ws://81.69.229.63";
+    const PROD_BASE_URL = "ws://119.45.173.104";
     const url = `${PROD_BASE_URL}/api/ws/picture/edit?pictureId=${this.pictureId}`
     this.socket = new WebSocket(url)
 
